4939b5ce4244fa3fba2185443e4c67eeeeb0a2c5
Mieszko Służewski <mieszkosluzewski@gmail.com>
2017-11-15 18:01:20 +0100

    Add tests and reverse sorting

--- algorithms/sorting.py
+++ algorithms/sorting.py
@@ -1,4 +1,4 @@
-def insertion_sort(data):
+def insertion_sort(data, reverse=False):
     """
     Simple implementation of insert_sort algorithm.
 
@@ -8,11 +8,24 @@ def insertion_sort(data):
     :return: sorted list
     """
 
-    for idx_1 in range(2, len(data)):
+    for idx_1 in range(1, len(data)):
         key = data[idx_1]
         idx_2 = idx_1 - 1
-        while idx_2 > 0 and data[idx_2] > key:
+
+        def check_key(idx):
+            """
+            Check if key is greater/smaller than item in idx position.
+
+            :param int idx: index of item
+
+            :rtype bool
+            :return True if key is greater and reverse is False or if key is smaller and reverse is True
+            """
+            return data[idx] > key if not reverse else data[idx] < key
+
+        while idx_2 >= 0 and check_key(idx_2):
             data[idx_2 + 1] = data[idx_2]
+            idx_2 -= 1
         data[idx_2 + 1] = key
 
     return data
